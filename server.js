ccba655bf214116a8fbdcd618ae0a00646757cc6
Jonatan_Eder_Campos <jonataneder.campos.a@gmail.com>
2025-07-29 09:59:27 -0600

    server_update2

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
